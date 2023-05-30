3d9d94de3ed5992fb6506e0370fab8e81cb2999d
chaquito7 <nicolsgonza765@gmail.com>
2023-05-30 18:30:41 -0300

    first commit

---
+++ src/main/java/com/mindhub/homebanking/MindhubBrothersApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.homebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindhubBrothersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindhubBrothersApplication.class, args);
+	}
+
+}
