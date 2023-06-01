08a9b56fcc1b6019ba0d2d1bc13cb93ca060ae99
chaquito7 <nicolsgonza765@gmail.com>
2023-06-01 12:45:30 -0300

    version 1.2.1

--- src/main/resources/static/manager.js
+++ src/main/resources/static/manager.js
@@ -3,18 +3,51 @@ const { createApp } = Vue
 const app = createApp({
   data() {
     return {
-      clients: []
+      clients:[],
+      json: "",
+      newclient: {name: "" ,lastName: "" ,email: ""}
     };
   },
   created() {
-    axios.get('http://localhost:8080/clients')
-      .then(response => {
-        this.clients = response.data;
-        console.log(this.clients);
+    this.loadData()
+  },
+  methods: {
+    loadData(){
+         axios.get('http://localhost:8080/clients')
+              .then(response => {
+                this.json = response.data;
+                this.clients = response.data._embedded.clients
+                console.log(this.json)
+                console.log(this.clients)
+              })
+              .catch(err => {
+                console.error(err);
+              });
+    },
+    addClients(){
+      if(this.newclient !== "") {
+        this.postClient()
+      }
+    },
+    postClient(){
+      axios.post("http://localhost:8080/clients", {
+        firstName: this.newclient.name,
+        lastName: this.newclient.lastName,
+        email: this.newclient.email
       })
-      .catch(error => {
-        console.error(error);
-      });
+      .then(res => {
+        this.loadData()
+        this.reset()
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+    },
+    reset(){
+      this.newclient.name = ""
+      this.newclient.lastName = ""
+      this.newclient.email = ""
+    }
   }
 })
 
