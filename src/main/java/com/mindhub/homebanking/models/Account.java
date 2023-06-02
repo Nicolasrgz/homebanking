42040e4390c8314a28b8592e9700513adba9b5ed
chaquito7 <nicolsgonza765@gmail.com>
2023-06-02 00:59:25 -0300

    version 1.3.1

---
+++ src/main/java/com/mindhub/homebanking/models/Account.java
@@ -0,0 +1,64 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.naming.Name;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String number;
+    private LocalDateTime creationDate;
+    private double balance;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name= "owner._id")
+    private Client owner;
+
+    public Account (){}
+
+    public Account(String number, LocalDateTime creationDate, double balance, Client owner) {
+        this.number = number;
+        this.creationDate = creationDate;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Client getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
+}
--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -1,10 +1,10 @@
 package com.mindhub.homebanking.models;
 
 import org.hibernate.annotations.GenericGenerator;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Client {
@@ -16,6 +16,13 @@ public class Client {
     private String firstName;
     private String lastName;
     private String email;
+
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+    Set<Account>accounts = new HashSet<>();
+
+    public  Set<Account> getAccounts(){
+        return accounts;
+    }
 
     public Client (){ }
 
