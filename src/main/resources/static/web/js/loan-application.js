1e7cffeb8afba6885f40e3c28435c12fec80d483
gonzalez  ramon nicolas <nicolsgonza765@gmail.com>
2023-07-07 13:30:03 -0300

    1.4

--- src/main/resources/static/web/js/loan-application.js
+++ src/main/resources/static/web/js/loan-application.js
@@ -1,26 +1,68 @@
 const {createApp} = Vue;
 
 const app = createApp({
-    data(){
-        return{
-            selectType: "",
-            selectPayments: "",
-            loansPayments: {},
-            loans: {},
-            amount: 0,
-        }
-    },
-    created(){
-        this.loansData()
-    },
-    methods:{
-        loansData(){
-            axios.get("http://localhost:8080/api/loans")
-            .then(res => {
-                this.loans = res.data
-                console.log(this.loans)
-            })
-        },
+  data() {
+    return {
+      selectType: "",
+      selectPayments: "",
+      selectAccount: "",
+      amount: 0,
+      loansPayments: {},
+      loans: {},
+      loansPaymentsMortgage: [],
+      loansPaymentsPersonnel: [],
+      loansPaymentsAutomotive: [],
+      client: [],
+      clientAccounts: [],
     }
+  },
+  created() {
+    this.loansData()
+    this.dataAccount()
+  },
+  mounted() {
+    new Cleave('#number', {
+      numeral: true,
+      numeralThousandsGroupStyle: 'thousand',
+      prefix: '$'
+    });
+  },
+  methods: {
+    loansData() {
+      axios.get("http://localhost:8080/api/loans")
+        .then(res => {
+          this.loans = res.data
+          this.loansPaymentsMortgage = this.loans[0].payments
+          this.loansPaymentsPersonnel = this.loans[1].payments
+          this.loansPaymentsAutomotive = this.loans[2].payments
+          console.log(this.loans)
+          console.log(this.loansPaymentsMortgage, this.loansPaymentsPersonnel, this.loansPaymentsAutomotive)
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+    dataAccount() {
+      axios.get("http://localhost:8080/api/clients/current")
+        .then(res => {
+          this.client = res.data
+          this.clientAccounts = this.client.accounts
+          console.log(this.clientAccounts)
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+    loanCreated(){
+        axios.post("http://localhost:8080/api/loans")
+        .then(res => {
+
+        })
+        .catch(err => {
+            console.error(err);
+          });
+    }
+  }
 })
+
 app.mount("#app")
