204874162eec5d79ba0eff285ddecc494280c30f
gonzalez  ramon nicolas <nicolsgonza765@gmail.com>
2023-07-12 23:30:08 -0300

    version 1.4

--- src/main/java/com/mindhub/homebanking/repositories/TransactionRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/TransactionRepository.java
@@ -1,6 +1,5 @@
 package com.mindhub.homebanking.repositories;
 
-import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/test/java/com/mindhub/homebanking/models/RepositoriesTest.java
+++ src/test/java/com/mindhub/homebanking/models/RepositoriesTest.java
@@ -1,7 +1,6 @@
 package com.mindhub.homebanking.models;
 
-import com.mindhub.homebanking.dtos.AccountDTO;
-import com.mindhub.homebanking.dtos.LoanDTO;
+import com.mindhub.homebanking.dtos.ClientDTO;
 import com.mindhub.homebanking.repositories.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +13,6 @@ import java.util.stream.Collectors;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
-import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = NONE)
@@ -27,10 +25,45 @@ public class RepositoriesTest {
     @Autowired
     CardRepository cardRepository;
     @Autowired
-    ClientLoanRepository clientLoanRepository;
-    @Autowired
     ClientRepository clientRepository;
     @Autowired
     TransactionRepository transactionRepository;
+
+    @Test
+    public void existLoans(){
+        List<Loan> loans = loanRepository.findAll();
+        assertThat(loans,is(not(empty())));
+    }
+
+    @Test
+    public void existMortgageLoan(){
+        List<Loan> loans = loanRepository.findAll();
+        assertThat(loans, hasItem(hasProperty("name", is("MORTGAGE"))));
+    }
+
+    @Test
+    public void existEmail(){
+        List<Client> email = clientRepository.findAll();
+        assertThat(email, hasItem(hasProperty("email", is("melba@mindhub.com"))));
+    }
+
+    @Test
+    public void getClient(){
+        List<ClientDTO> clientDTOS = clientRepository.findAll().stream().map(ClientDTO::new).collect(Collectors.toList());
+        assertThat(clientDTOS,is(not(empty())));
+    }
+
+    @Test
+    public void existNumberCard(){
+        List<Card> number= cardRepository.findAll();
+        assertThat(number, hasItem(hasProperty("number", is("0857-6181-1934-5445"))));
+    }
+
+    @Test
+    public void existColorCard(){
+        List<Card> color = cardRepository.findAll();
+        assertThat(color, hasItem(hasProperty("color", is(CardColor.GOLD))));
+    }
+
 
 }
