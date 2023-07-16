d00fc6cddb675b9ee58a32ccba0423cbe4b218c9
gonzalez  ramon nicolas <nicolsgonza765@gmail.com>
2023-07-16 10:44:48 -0300

    version 3

--- src/main/resources/static/web/js/loan-admin.js
+++ src/main/resources/static/web/js/loan-admin.js
@@ -3,14 +3,29 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return{
-
+            name: "",
+            maxAmount: null,
+            payments: []
         }
     },
     created(){
 
     },
+    mounted() {
+        new Cleave('#amount', {
+          numeral: true,
+          numeralThousandsGroupStyle: 'thousand',
+          prefix: '$'
+        });
+      },
     methods:{
+        createdLoan(){
+            axios.post("http://localhost:8080/api/loans/created")
+            .then(res => {
 
+            })
+            .catch(err => console.error(err))
+        }
     },
 })
 app.mount("#app")
